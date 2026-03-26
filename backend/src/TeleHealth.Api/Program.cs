fc7cf004cd93370db472ed2b22d74ab4d0f3b108
KhooLayHan <khoolayhan@github.com>
2026-03-27 06:01:42 +0800

    feat: added serilog with OpenTelemetry integration, part 1

--- backend/src/TeleHealth.Api/Program.cs
+++ backend/src/TeleHealth.Api/Program.cs
@@ -1,11 +1,46 @@
 using Microsoft.EntityFrameworkCore;
+
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
 using Scalar.AspNetCore;
+
+using Serilog;
+using Serilog.Sinks.OpenTelemetry;
+
 using TeleHealth.Api;
 using TeleHealth.Api.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.OpenTelemetry(x =>
+    {
+        x.Endpoint = "http://telehealth_seq:5341/ingest/otlp/v1/logs";
+        x.Protocol = OtlpProtocol.HttpProtobuf;
+        x.Headers.Add("X-Seq-ApiKey", "DOjvrcXB9RfyEGDwyKX2");
+        x.ResourceAttributes.Add("service.name", "TeleHealth.Api");
+    })
+    .CreateLogger();
+
+builder.Services.AddSerilog();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("TeleHealth.Api"))
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation().AddAWSInstrumentation().AddHttpClientInstrumentation();
+    }).WithTracing(tracing =>
+    {
+        tracing.AddAspNetCoreInstrumentation().AddAWSInstrumentation().AddHttpClientInstrumentation();
+    });
+
+builder.Logging.AddOpenTelemetry(logging => logging.AddOtlpExporter());
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -19,7 +54,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -36,5 +70,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseSerilogRequestLogging();
 
 await app.RunAsync();
