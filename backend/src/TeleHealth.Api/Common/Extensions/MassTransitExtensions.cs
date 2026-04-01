af7c2a25bd6018d47498cc405fb33d583b2e3487
KhooLayHan <khoolayhan@github.com>
2026-04-01 10:38:08 +0800

    feat: added outbox MassTransit migrations

--- backend/src/TeleHealth.Api/Common/Extensions/MassTransitExtensions.cs
+++ backend/src/TeleHealth.Api/Common/Extensions/MassTransitExtensions.cs
@@ -7,7 +7,7 @@ namespace TeleHealth.Api.Common.Extensions;
 public static class MassTransitExtensions
 {
     public static IServiceCollection AddMassTransitConfiguration(
-        this IServiceCollection services
+        this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment
     )
     {
         services.AddMassTransit(x =>
@@ -18,12 +18,31 @@ public static class MassTransitExtensions
                 o.UseBusOutbox();
                 o.QueryDelay = TimeSpan.FromSeconds(1);
             });
-
-            x.UsingAmazonSqs((ctx, cfg) =>
+            
+            // Dev
+            if (environment.IsDevelopment())
             {
-                cfg.LocalstackHost();
-                cfg.ConfigureEndpoints(ctx);
-            });
+                x.UsingAmazonSqs((ctx, cfg) =>
+                {
+                    cfg.LocalstackHost();
+                    cfg.ConfigureEndpoints(ctx);
+                });    
+            }
+            else
+            {
+                // Prod
+                x.UsingAmazonSqs((ctx, cfg) =>
+                {
+                    cfg.Host(configuration["Aws:Region"]!,  h =>
+                    {
+                        h.AccessKey(configuration["AWS:AccessKey"]!);
+                        h.SecretKey(configuration["AWS:SecretKey"]!);
+                    });
+                });
+            }
+
+            
+
         });
 
         return services;
