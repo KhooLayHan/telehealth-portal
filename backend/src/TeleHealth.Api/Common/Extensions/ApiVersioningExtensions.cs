f89911d5fc26cc2443228d5ba267ad00741bfc1f
KhooLayHan <khoolayhan@github.com>
2026-04-02 10:22:12 +0800

    feat: added api versioning

---
+++ backend/src/TeleHealth.Api/Common/Extensions/ApiVersioningExtensions.cs
@@ -0,0 +1,40 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+
+namespace TeleHealth.Api.Common.Extensions;
+
+public static class ApiVersioningExtensions
+{
+    public static IServiceCollection AddApiVersioningConfiguration(this IServiceCollection services)
+    {
+        services
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'V";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        return services;
+    }
+
+    public static RouteGroupBuilder CreateVersionedApiGroup(
+        this IEndpointRouteBuilder app,
+        int majorVersion,
+        int minorVersion = 0
+    )
+    {
+        var version = new ApiVersion(majorVersion, minorVersion);
+        var versionSet = app.NewApiVersionSet().HasApiVersion(version).ReportApiVersions().Build();
+
+        return app.MapGroup($"api/v{{version:apiVersion}}")
+            .WithApiVersionSet(versionSet)
+            .MapToApiVersion(version);
+    }
+}
